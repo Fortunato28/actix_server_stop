5842ca1e8bd06db12eae71a4c56917dfad122251
Anton Sapunov <fort.sav.28@gmail.com>
2021-06-03 21:24:11 +0700

    Server stopping properly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{dev::Server, get, post, rt, web, App, HttpResponse, HttpServer, Responder};
 use anyhow::{anyhow, Result};
+use futures::executor;
 use parking_lot::Mutex;
-use std::{sync::Arc, thread, time::Duration};
+use std::{sync::mpsc, sync::Arc, thread, time::Duration};
 use tokio::sync::oneshot;
 
 #[get("/health")]
@@ -11,31 +12,47 @@ async fn health() -> &'static str {
 }
 
 #[post("/stop")]
-async fn stop(control_panel: web::Data<Arc<ControlPanel>>) -> impl Responder {
-    let stop_receiver = control_panel.get_ref().clone().stop();
-    let _outcome = stop_receiver.await;
-    dbg!(&"STOP AWAITED");
+async fn stop(server_stopper: web::Data<mpsc::Sender<()>>) -> impl Responder {
+    dbg!(&"STOP ENDPOINT");
+    server_stopper.send(()).expect("Problem in send");
 
     HttpResponse::NoContent().finish()
 }
 
 struct ControlPanel {
     address: String,
-    server: Arc<Mutex<Option<Server>>>,
+    tx: Arc<Mutex<Option<mpsc::Sender<()>>>>,
 }
 
 impl ControlPanel {
     pub(crate) fn new(address: &str) -> Arc<Self> {
         Arc::new(Self {
             address: address.to_owned(),
-            server: Arc::new(Mutex::new(None)),
+            tx: Arc::new(Mutex::new(None)),
         })
     }
 
     /// Start Actix Server in new thread
     pub(crate) fn start(self: Arc<Self>) {
+        let (tx, rx) = mpsc::channel::<()>();
+        *self.tx.lock() = Some(tx.clone());
+        let server =
+            HttpServer::new(move || App::new().data(tx.clone()).service(health).service(stop))
+                .bind(&self.address)
+                .expect("Unable to bind")
+                .shutdown_timeout(1)
+                .workers(1)
+                .run();
+
+        let cloned_server = server.clone();
         thread::spawn(move || {
-            if let Err(error) = self.start_server() {
+            rx.recv().unwrap();
+
+            executor::block_on(cloned_server.stop(false));
+        });
+
+        thread::spawn(move || {
+            if let Err(error) = self.start_server(server) {
                 dbg!("Unable start rest api server: {}", error.to_string());
             }
         });
@@ -43,51 +60,19 @@ impl ControlPanel {
 
     /// Stop Actix Server if it is working.
     /// Returned receiver will take a message when shutdown are completed
-    fn stop(self: Arc<Self>) -> tokio::sync::oneshot::Receiver<Result<()>> {
-        let (tx, rx) = oneshot::channel();
-
-        let cloned_self = self.clone();
-        let runtime_handler = tokio::runtime::Handle::current();
-        thread::spawn(move || {
-            let maybe_server = cloned_self.server.lock();
-
-            match &(*maybe_server) {
-                Some(server) => runtime_handler.block_on(async {
-                    server.stop(false).await;
-
-                    dbg!(&"SERVER STOPPED");
-                    let _ = tx.send(Ok(()));
-                }),
-                None => {
-                    let error_message =
-                        "Unable to stop ControlPanel because server is not runnning";
-                    dbg!("{}", error_message);
-                    let _ = tx.send(Err(anyhow!(error_message)));
-                }
-            }
-        });
-
-        rx
+    fn stop(self: Arc<Self>) {
+        self.tx
+            .lock()
+            .clone()
+            .expect("There are no tx in structure")
+            .send(())
+            .expect("Unable_to_send_message");
     }
 
-    fn start_server(self: Arc<Self>) -> std::io::Result<()> {
-        let address = self.address.clone();
-
-        let cloned_self = self.clone();
+    fn start_server(self: Arc<Self>, server: Server) -> std::io::Result<()> {
         let system = Arc::new(rt::System::new());
-        let server = HttpServer::new(move || {
-            App::new()
-                .data(cloned_self.clone())
-                .service(health)
-                .service(stop)
-        })
-        .bind(&address)?
-        .shutdown_timeout(1)
-        .workers(1);
 
-        system.block_on(async {
-            *self.server.lock() = Some(server.run());
-        });
+        system.block_on(server);
 
         Ok(())
     }
@@ -95,7 +80,8 @@ impl ControlPanel {
 
 #[actix_web::main]
 async fn main() {
-    ControlPanel::new("127.0.0.1:8080").start();
+    let control_panel = ControlPanel::new("127.0.0.1:8081");
+    control_panel.clone().start();
 
     tokio::time::sleep(Duration::from_secs(20)).await;
 }
